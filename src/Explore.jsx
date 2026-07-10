4f3155fe19adf6df6957fda0307b1622fe015354
Trinity Ndlovu <ndlovutrinity18@gmail.com>
2026-07-10 14:21:23 +0200

    Add hero section to Explore component and update Navbar styles

--- src/Explore.jsx
+++ src/Explore.jsx
@@ -1,7 +1,12 @@
+import "./App.css";
 export default function Explore() {
   return (
-    <div>
-      <h1>Explore</h1>
+    <div className="explore">
+        <div className="hero">
+      <h1>Discover your next destination.</h1>
+      <p>Describe your dream trip — Terra finds destinations that match you.</p>
+      </div>
+      <p>SEE YOU IN THE CLOUD</p>
     </div>
   )
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,8 +6,8 @@ export default function Navbar() {
     <div className="navbar">
     <ul class="nav justify-content-end">
       <li class="nav-home">
-        <image src={image} alt="logo" width="30" height="30" />
-    <a class="nav-link" href="#">Terra.T</a>
+        <img className="nav-logo" src={image} alt="logo" width="30" height="30" />
+    <a class="nav-link" href="#">TERRA.T</a>
   </li>
   <li class="nav-item">
     <a class="nav-link active" aria-current="page" href="#">Explore</a>
