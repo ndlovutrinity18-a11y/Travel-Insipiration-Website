0e8338bf40cb65b377eed84e335a5a2ed5d6f0db
Trinity Ndlovu <ndlovutrinity18@gmail.com>
2026-07-31 18:33:43 +0200

    Update Navbar and Explore component styles, add dropdown menu, and integrate react-router-dom

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,39 +1,76 @@
 
 import './Navbar.css';
+import {useState} from 'react';
 import image from './Terra-logo.png';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 export default function Navbar() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   return (
     <div className="navbar">
-    <ul class="nav justify-content-end">
-      <li class="nav-home">
+    <ul className="nav-list">
+      <li className="nav-home">
         <img className="nav-logo" src={image} alt="logo" width="30" height="30" />
         <Link to="/" className="nav-link">
           TERRA.T
         </Link>
       </li>
-      <li class="nav-item">
-        <Link to="/explore" className="nav-link active" aria-current="page">
+      <li className="nav-item dropdown">
+        <button 
+          className="nav-link dropdown-btn" 
+          onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+        >
           Explore
-        </Link>
-      </li>
-      <li class="nav-item">
+        </button>
+        {isDropdownOpen && (
+        <ul className="dropdown-menu">
+          <li>
+            <Link to="/explore/last-minute" className="dropdown-link">
+              Last Minute Travel
+            </Link>
+          </li>
+          <li>
+            <Link to="/explore/friends" className="dropdown-link">
+              Friends Getaways
+            </Link>
+          </li>
+          <li>
+            <Link to="/explore/honeymoon" className="dropdown-link">
+              Honeymoon Destinations
+            </Link>
+          </li>
+          <li>
+            <Link to="/explore/beach" className="dropdown-link">
+              Beach Escapes
+            </Link>
+          </li>
+          <li>
+            <Link to="/explore/safari" className="dropdown-link">
+              Safari Adventures
+            </Link>
+          </li>
+          <li>
+            <Link to="/explore/budget" className="dropdown-link">
+              Budget-Friendly Getaways
+            </Link>
+          </li>
+        </ul>
+        )}
+      <li className="nav-item">
         <Link to="/favourites" className="nav-link">
           Favourites
         </Link>
       </li>
-      <li class="nav-item">
+      <li className="nav-item">
         <Link to="/visa-free" className="nav-link">
           Visa-Free
         </Link>
       </li>
-      <li class="nav-item">
-        <Link to="/about" className="nav-link disabled">
-          About
-        </Link>
+      <li className="nav-item">
+        <button type="button" className="btn btn-outline-light">Saved(0)
+        </button>
       </li>
-      <button type="button" class="btn btn-outline-light">Saved(0)</button>
+      </li>
 </ul>
-    </div>
+ </div>
   )
 }
