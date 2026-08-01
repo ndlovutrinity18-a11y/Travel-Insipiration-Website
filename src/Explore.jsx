fe6627ce9693eba090afbfebf41d610fd5b322bf
Trinity Ndlovu <ndlovutrinity18@gmail.com>
2026-08-01 16:23:09 +0200

    Add Hero component and integrate it into Explore page

--- src/Explore.jsx
+++ src/Explore.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Hero from "./Hero";
 import Navbar from "./Navbar";
 export default function Explore() {
   return (
     <div className="explore">
       <Navbar />
+      <Hero />
       <p>SEE YOU IN THE CLOUD</p>
     </div>
   )
---
+++ src/Hero.jsx
@@ -0,0 +1,8 @@
+ export default function Hero() {
+  return (
+    <div className="hero">
+      <h1>Welcome to TERRA.T</h1>
+      <p>Your ultimate travel inspiration destination</p>
+    </div>
+  );
+}
